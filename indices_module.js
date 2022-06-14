aaf77dd19db90a697d84f7b9bae65224fa611fd7
markb500 <81427572+markb500@users.noreply.github.com>
2022-06-14 18:55:54 +0100

    Add files via upload

--- indices_module.js
+++ indices_module.js
@@ -51,9 +51,11 @@ function indices() {
         xn = p3d * r3 * (p1n * p2d * r2 + p2n * p1d * r1) - p3n * p1d * r1 * p2d * r2;
         xd = p1d * r1 * p2d * r2 * p3d * r3;
     }while(Math.abs(xn) > 35 || Math.abs(xd) > 10 || Math.abs(xn) === Math.abs(xd) || xn === 0 || xd === 0 || 
-            (p1d * r1) === (p2d * r2) || (r1 === r3 && p1n === p3n && p1d === p3d) || (r1 === r2 && p1n === p2n && p1d === p2d))
-            //Sets difficulty level (max numerator & max denominator), avoids same numerator & denominator or either being 0,
-            //avoids term1 & term2 denoms being same, avoids term1 & term3 being the same and avoids term1 & term 2 being the same.
+            (p1d * r1) === (p2d * r2) || Math.abs(p1n) === Math.abs(r1 * p1d) || Math.abs(p2n) === Math.abs(r2 * p2d) || Math.abs(p3n) === Math.abs(r3 * p3d) || 
+            Math.abs(p1n / (r1 * p1d)) === Math.abs(p3n / (r3 * p3d)) || Math.abs(p2n / (r2 * p2d)) === Math.abs(p3n / (r3 * p3d)))
+                    //Sets difficulty level (max numerator & max denominator), avoids same numerator & denominator or either being 0,
+                    //avoids term1 & term2 denoms being same and any term numerator equalling its denominator.
+                    //Also avoides term 1 or term 2 indexes equalling term 3 index (easy cancel)
 
     comfac = gcd2(xn, xd);      //Ensures the solution index fraction is in the simplest form
     while(comfac !== 1) {
