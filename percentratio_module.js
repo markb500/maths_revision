391c5834afba9b8f7cc75bb7a18a1e0408b096d5
markb500 <81427572+markb500@users.noreply.github.com>
2022-06-20 21:22:58 +0100

    Add files via upload

--- percentratio_module.js
+++ percentratio_module.js
@@ -1,5 +1,5 @@
 var prevsum = 0, prev2sum = 0, sumq, suma, num1, num2, pct, rat1, rat2, rat3;
-var ratunits = [" m", " N", " kg", "£", "\\ m", "\\ N", "\\ kg", "£"], iratunit;
+var ratunits = ["&nbsp;m", "&nbsp;N", "&nbsp;kg", "£", "\\ m", "\\ N", "\\ kg", "£"], iratunit;
 function percentratio() {
     //Randomly selects from 6 questions (5 percent & 1 ratio). Units randomly changed in ration question.
     var sum, comdenom;
@@ -39,7 +39,7 @@ function percentratio() {
                 pct = rndgen(3, 99, 0, 1, -1);
             } while(pct === 10 || pct === 25 || pct === 50 || pct === 75)
             num1 = rndgen(16, 51, 0, 1, -1);
-            sumq += "Calculate, without using a calculator, " + pct + " % of " + num1 + 
+            sumq += "Calculate, without using a calculator, " + pct + "&nbsp;% of " + num1 + 
                         ", rounding your answer to 1 decimal place.";
             suma += "$$\\frac{" + pct + "}{100}\\times" + num1 + "=" + 
                         dp((pct / 100) * num1, 2, 1) + "\\ (1\\ dp)$$";
@@ -50,7 +50,7 @@ function percentratio() {
             }
             pct = rndgen(3, 99, 0, 1, -1);
             comdenom = gcd2(pct, 100);
-            sumq += "Without using a calculator, express " + pct + " % as a proper fraction in its simplest form.";
+            sumq += "Without using a calculator, express " + pct + "&nbsp;% as a proper fraction in its simplest form.";
             if(comdenom !== 1) {
                 suma += "$$\\frac{" + pct + "}{100}=\\frac{" + (pct / comdenom) + "}{" + (100 / comdenom) + "}$$";
             } else {
@@ -62,7 +62,7 @@ function percentratio() {
                 window.open("images/20200427-MathsBook1BasicNumv1_3-APO.pdf#page=50", "_blank")
             }
             pct = rndgen(3, 99, 1, 0.1, -1);
-            sumq += "Without using a calculator, convert " + pct + " % to a decimal.";
+            sumq += "Without using a calculator, convert " + pct + "&nbsp;% to a decimal.";
             suma += "$$\\frac{" + pct + "}{100}=" + dp(pct / 100, 3, -1) + "$$"; 
             break;
         case 5://Without using a calculator, convert Round(p / 100, 4) to a percentage."""
